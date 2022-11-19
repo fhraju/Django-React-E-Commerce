b9d3ab5aaf52aacedd43f6f80382cb4405ea5f9e
fhraju <alonghuman@gmail.com>
2022-11-20 00:05:31 +0600

    Backend Created

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import axios from 'axios';
 
 import Rating from '../components/Rating'
-import products from '../products'
+
 
 function ProductScreen() {
-    let params = useParams()
-    const product = products.find((p) => p._id === params.id )
+    const params = useParams()
+
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        async function fetchProduct() {
+        const { data } = await axios.get(`/api/products/${params.id}`)
+        setProduct(data)
+    }
+
+    fetchProduct()
+  }, [])
+
     return (
     <div>
         <Link to='/' className="btn btn-primary my-3">Go Back</Link>
