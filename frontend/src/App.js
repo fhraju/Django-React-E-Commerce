e50c986d9884a51e9bdec290b57aef84598b734e
fhraju <alonghuman@gmail.com>
2022-11-19 16:24:40 +0600

    ProductScreen.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import { Container } from 'react-bootstrap'
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HoomeScreen from './screens/HoomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Container>
-          <HoomeScreen />
+          <Routes>
+            <Route path="/" element={<HoomeScreen />} exact/>
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
